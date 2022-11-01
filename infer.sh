3ae2c0def4a6908caabd694bd840d0b13f5af2a6
学习之父贝叶斯 <70255489+iClaude1998@users.noreply.github.com>
2022-11-01 22:50:33 +1100

    change the inference command for tusimple

--- infer.sh
+++ infer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo tusimple_abs_root: $1
+echo dataroot: $1
 
-python test.py --dataroot "suibian" --name summer2winter_yosemite_pretrained --model test --no_dropout --tusimple --tusimpleABSroot $1 --num_test 1000000 --preprocess none
+python test.py --dataroot $1 --name summer2winter_yosemite_pretrained --model test --no_dropout --llamas --num_test 66666 --preprocess none
 
-
-mv results/summer2winter_yosemite_pretrained/test_latest/images/style/clips/* $1
+mkdir  $1/winter_style
+mv results/summer2winter_yosemite_pretrained/test_latest/images/style/color_images/* $1/winter_style
