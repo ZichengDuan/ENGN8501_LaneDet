87babff188e20aab60b48227e752875e043ddb5c
poppingg123 <poppingg123@gmail.com>
2022-11-01 11:44:14 +0000

    x

--- infer.sh
+++ infer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo tusimple_abs_root: $1
 
-python test.py --dataroot "suibian" --name summer2winter_yosemite_pretrained --model test --no_dropout --tusimple --tusimpleABSroot $1 --num_test 6666
+python test.py --dataroot "suibian" --name summer2winter_yosemite_pretrained --model test --no_dropout --tusimple --tusimpleABSroot $1 --num_test 1000000 --preprocess none
 
 
 mv results/summer2winter_yosemite_pretrained/test_latest/images/style/clips/* $1
