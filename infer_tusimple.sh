d1e3bc5083bc2e63fa3e9ee118d728cca8982594
iClaude1998 <1316761893@qq.com>
2022-11-02 21:18:00 +1100

    change the infer_tusimple.sh

--- infer_tusimple.sh
+++ infer_tusimple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo dataroot: $1
 
-python test.py --dataroot $1 --name summer2winter_yosemite_pretrained --model test --no_dropout --tusimple --num_test 6666
+python test.py --dataroot $1 --name summer2winter_yosemite_pretrained --model test --no_dropout --tusimple --num_test 6666 --preprocess none
 
 
-mv results/summer2winter_yosemite_pretrained/test_latest/images/style/clips/* $1
+mv results/summer2winter_yosemite_pretrained/test_latest/images/style/clips/* $1/winter_style
